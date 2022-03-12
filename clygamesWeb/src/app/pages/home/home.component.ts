aecec1ebe3d088beba4386b734af3d577a6818c9
nniecor <nuriani@gmail.com>
2022-03-12 22:36:39 +0100

    traduccion, arreglo de scroll

--- clygamesWeb/src/app/pages/home/home.component.ts
+++ clygamesWeb/src/app/pages/home/home.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public url: string;
+  public link: any;
+  private idioma: string;
 
-  ngOnInit(): void {
+  constructor(private translate: TranslateService, private sanitizer: DomSanitizer) {
+    this.url = this.crearUrl();
+    this.idioma = this.translate.currentLang ? this.translate.currentLang : translate.getDefaultLang();
+    console.log(this.idioma);
+
+
   }
 
+  ngOnInit(): void {
+
+  }
+
+  crearUrl() {
+    if ( this.idioma === "es" ) {
+      this.url = "https://www.youtube.com/embed/fV8GvTlqcug";
+    } else if ( this.idioma === "en" ){
+      this.url = "https://www.youtube.com/embed/2dbaDD46XZ4";
+    }
+
+    this.link = this.sanitizer.bypassSecurityTrustResourceUrl(this.url)
+    return this.link;
+  }
 }
